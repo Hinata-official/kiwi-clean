c6ee69c74f451c18e231cd22e64cc04a0a5acd42
wenweneqi <61258915+wenweneqi@users.noreply.github.com>
2022-08-07 20:54:13 +0800

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,15 @@
+      rm -rf /data/data/com.huya.kiwi/files/.props
+      rm -rf /data/data/com.huya.kiwi/files/huyaudb/logcache0
+      rm -rf /data/data/com.duowan.kiwi/files/.umeng
+      rm -rf /data/data/com.duowan.kiwi/files/splash
+      rm -rf /data/data/com.duowan.kiwi/files/huyaudb
+      rm -rf /data/data/com.duowan.kiwi/files/.props
+      rm -rf /data/data/com.duowan.kiwi/files/huyaudb/logcache0
+      rm -rf /data/user/0/com.duowan.kiwi/files/.props
+      rm -rf /data/user/0/com.duowan.kiwi/files/.umeng
+      rm -rf /data/user/0/com.duowan.kiwi/files/live_log
+      rm -rf /data/user/0/com.duowan.kiwi/files/splash
+      rm -rf /data/user/0/com.duowan.kiwi/files/huyaudb/logcache0
+      rm -rf /data/user/0/com.duowan.kiwi/files/com.duowan.kiwi/logging_cache
+      mv -f /sdcard/Huya/syshosts.bak /system/etc/hosts
+      rm -rf /sdcard/Android/Huya
